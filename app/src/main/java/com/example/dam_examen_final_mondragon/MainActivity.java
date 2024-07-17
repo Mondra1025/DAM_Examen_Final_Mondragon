4416e70b5d36511c5de327987e52257c832015f6
Alberto Mondragon <ismael20_mondragon@hotmail.com>
2024-07-17 10:49:31 -0500

    Nota en columnas creado

--- app/src/main/java/com/example/dam_examen_final_mondragon/MainActivity.java
+++ app/src/main/java/com/example/dam_examen_final_mondragon/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.dam_examen_final_mondragon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotaActivity.class);
+                startActivity(i);
+            }
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/dam_examen_final_mondragon/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.dam_examen_final_mondragon;
+
+public interface NotasInteractionListener {
+    void editNotaClick (Nota nota);
+    void eliminarNotaClick(Nota nota);
+    void favoritaNotaClick(Nota nota);
+}
